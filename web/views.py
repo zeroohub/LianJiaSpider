c9b6e52ab1b7aa2d2c798ad82a24f84a5543eb0e
DorianWang <zhaoyu.wang@ele.me>
2017-07-03 17:08:34 +0800

    fixup

--- web/views.py
+++ web/views.py
@@ -3,18 +3,12 @@ from __future__ import unicode_literals
 
 from datetime import datetime, timedelta
 from django.shortcuts import render
-
 # Create your views here.
 from web.models import Apartment
 
 
 def arts_list(request):
-    apts = Apartment.objects.filter(
+    apts = Apartment.good_room().filter(
         online_time__gte=datetime.now() - timedelta(days=1)
-    ).filter(
-        rent_price__lte=4000
-    ).filter(
-        rent_city_code='shz'
-    ).order_by('rent_price')
-
+    )
     return render(request, 'web/index.html', {'apts': apts})
