62aa7cd31eb3a79419fdb062be0f830c6bfccf82
dorian <spamzero@yeah.net>
2017-07-03 15:14:33 +0800

    opt email

--- web/crons.py
+++ web/crons.py
@@ -3,31 +3,29 @@ from __future__ import unicode_literals
 import json
 
 from django.core.mail import send_mail
-from django.forms.models import model_to_dict
+from django.template.loader import get_template
 
 from models import Apartment
 from datetime import datetime, timedelta
 
 
 def email_new_apartment():
+
     apts = Apartment.objects.filter(
         online_time__gte=datetime.now() - timedelta(days=1)
     ).filter(
         rent_price__lte=4000
-    )
-    apts_dict = []
-    for a in apts:
-        na = model_to_dict(a)
-        for k, v in na.iteritems():
-            if isinstance(v, datetime):
-                na[k] = v.isoformat()
-        apts_dict.append(na)
+    ).filter(
+        rent_city_code='shz'
+    ).order_by('rent_price')
 
+    template = get_template('web/index.html')
+    result = template.render({'apts': apts})
 
     send_mail(
         'LianJia New Room Available',
-        json.dumps(apts_dict),
-        '',
-        [''],
+        from_email='',
+        recipient_list=[''],
         fail_silently=False,
+        html_message=result
     )
